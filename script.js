bc2ec60bcddc901e1551f4367d81271f1f19fbc0
michellesambuu <michelle.sambuu@gmail.com>
2023-03-02 10:24:04 -0600

    addad while loop to detmine pas loop

--- script.js
+++ script.js
@@ -15,7 +15,19 @@
 // THEN the password is either displayed in an alert or written to the page
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-function generatePassword(){}
+var numbers= [1,2,3,4,5,6,7,8,9,0]
+var specialCharcters=[ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ", ' " ', "\\" ]
+var lowerCase=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var upperCase=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var pwLength=0
+var result=""
+var choices=[]
+
+function generatePassword(){
+  while(pwLength<8||pwLength>128||pwLength===""||isNaN(pwLength)){
+    pwLength=prompt("Please choose a password length between 8-128 characters")
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
